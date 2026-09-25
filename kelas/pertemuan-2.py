1f78c3846c4865a0f40323999ab67bfeec885910
Kamil Rakhadian <kamilrakhadian02@gmail.com>
2026-09-25 09:28:30 +0800

    Update

---
+++ kelas/pertemuan-2.py
@@ -0,0 +1,59 @@
+# #angka = 6 
+# #if angka < 10: # Kondisi percabangan IF 
+# #    print("Angka kurang dari 10")
+
+# # umur = int(input("Masukkan umur: ")) # Input umur 
+# # # Misalkan, umur = 17 
+# # if umur >= 17: 
+# #     print("Kamu sudah bisa membuat KTP") # Blok if dijalankan karena kondisi True 
+# # else: 
+# #     print("Kamu belum bisa membuat KTP") # Blok else tidak dijalankan 
+
+# # Input jenis kendaraan dari user 
+# kendaraan = input("Masukkan jenis kendaraan anda: ") 
+# # Misalnya, kendaraan = "mobil" 
+# # Percabangan
+# if kendaraan == "mobil": 
+#     tarif_parkir = 10000 
+# elif kendaraan == "motor": 
+#     tarif_parkir = 5000 
+# else: 
+#     tarif_parkir = 15000 
+
+# # Menampilkan tarif parkir yang harus dibayar  
+# print("Tarif parkir yang harus dibayar:", tarif_parkir)  
+
+# Bentuk Awal Percabangan IF/ELSE 
+# umur = 20 
+# if umur >= 18: 
+#     status = "Dewasa" 
+# else: 
+#     status = "Belum Dewasa" 
+# Bentuk Ternary Operator 
+# umur = 20
+# status = "Dewasa" if umur >= 18 else "Belum Dewasa" 
+
+#nested IF
+# nilai = int(input("Masukkan nilai: "))
+
+# if nilai >= 10:
+#     if nilai >= 20:
+#         if nilai >= 30:
+#             print("Angka besar")
+#         print("Angka sedang")
+#     print("Angka kecil")
+
+# usia = int(input("Masukkan usia: ")) else print("Maaf, anda belum cukup umur") if usia >= 16 print("Selamat mengikuti acara kami") 
+#perbaikin nanti
+
+# buy = int(input("Masukkan total belanja: "))
+# if buy > 200000:
+#     diskon = 0.3 * buy
+#     total = buy - diskon
+# elif buy > 100000:
+#     diskon = 0.1 * buy
+#     total = buy - diskon
+# else:
+#     diskon = 0
+#     total = buy
+# print("Total belanja: ", "Rp.", total)
--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-print("Hello")
-print("Miaw Miaw Miaw") 
+# # print("Hello")
+# # print("Miaw Miaw Miaw") 
+
+diskon1 = 0.1
+diskon2 = 0.05
+diskon3 = 0
+diskon = [diskon1, diskon2, diskon3]
+liter = int(input("Masukkan jumlah liter: "))
+if liter >= 10:
+    diskon = diskon[0]
+elif liter >= 5:
+    diskon = diskon[1]
+else:
+    diskon = diskon[2]
+print("Diskon yang didapatkan: ", diskon * 100, "%")
+total_harga = liter * 10000
+diskon_h = diskon * total_harga
+total_bayar = total_harga - diskon_h
+print("Total harga yang harus dibayar: ", int(total_bayar)) 
+diskon_member = input("Apakah anda member? (y/n): ")
+if diskon_member == "y":
+    diskon_m1 = 0.02
+    diskon_m2 = diskon_m1 * total_harga
+    diskon_g = diskon_m2 + diskon_h
+    print("Diskon yang didapatkan member: ", int((diskon_m1 + diskon) * 100), "%")
+    total_bayar_akhir = total_harga - diskon_g
+    print("Total harga setelah diskon member: ", int(total_bayar_akhir)) 
+else:
+    print("Terima kasih telah berbelanja di toko kami!")
+    print("Diskon yang didapatkan           : ", diskon * 100, "%")
+    print("Total harga yang harus dibayar   : ", int(total_bayar)) 
+    exit()
+
+print("Terima kasih telah berbelanja di toko kami!")
+print("Diskon yang didapatkan               : ", diskon * 100, "%")
+print("Total harga yang harus dibayar       : ", int(total_bayar)) 
+print("Diskon yang didapatkan member        : ", int((diskon_m1 + diskon) * 100), "%")
+print("Total harga setelah diskon member    : ", int(total_bayar_akhir))
